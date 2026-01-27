65960fa3f86926a1b35d437ddf2acba5d9e98847
Rick13211 <rick13211roy@gmail.com>
2026-01-28 02:04:08 +0530

    edited middleware file

--- middleware.ts
+++ middleware.ts
@@ -16,5 +16,15 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|share).*)'],
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   * - auth (authentication pages)
+   * - share (public notes)
+   * - The root path (/) [Note the '$' at the end]
+   */
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|auth|share|$).*)'],
 };
